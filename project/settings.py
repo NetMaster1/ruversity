9ff385f9da033fa2f5bbbbfc5b4debd5e6986183
Sergey Vinokurov <79200711112@yandex.ru>
2022-05-05 12:09:50 +0300

    Changed DB for postresql

--- project/settings.py
+++ project/settings.py
@@ -85,13 +85,25 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':   'django.db.backends.postgresql',
+            'NAME': 'ruversity',
+            'USER': 'postgres',
+            'PASSWORD': 'ylhio65v',
+            'HOST': 'localhost',
+            'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
