d4fc8b2b37c42126cfa42bbad47f289cd404b0c9
Sergey Vinokurov <79200711112@yandex.ru>
2022-08-29 15:27:59 +0300

    Revert "deleted settings from git"
    This reverts commit 4e2035b2c666bde47ff87ecf9060c2f65929ef6b.

--- app_workshop/views.py
+++ app_workshop/views.py
@@ -598,9 +598,7 @@ def agree(request, subject_id):
                 subject.ready=True
                 subject.save()
             for lecture in lectures:
-                #========DRM feature====================
                 lecture.processing_state = PROCESSING_READY_TO_START
-                #============================================
                 lecture.save()
                 if badwords:
                     for word in badwords:
