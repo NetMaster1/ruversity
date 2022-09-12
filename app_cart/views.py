503c0768d9748b5c4d58b9875166f028813a899a
Sergey Vinokurov <79200711112@yandex.ru>
2022-09-12 04:47:42 +0300

    remote

--- app_cart/views.py
+++ app_cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from app_content.models import MainSubject, Transaction, Cart, DiscountOn
+
 from hashlib import md5
 from django.http import HttpResponse
 import json
