c63e5ddaad81734bdf0a100a141a3aedf071df8b
Sergey Vinokurov <79200711112@yandex.ru>
2022-10-30 14:24:57 +0300

    edited admin

--- app_content/admin.py
+++ app_content/admin.py
@@ -12,6 +12,7 @@ class LectureAdmin(admin.ModelAdmin):
     list_display = ('id', 'subject', 'title', 'video_file', 'author', 'date_posted', 'size_mb', 'length', 'length_1')
     ordering = ('-id',)
     list_per_page=100
+    list_filter = ('subject',)
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('id', 'date_created', 'course', 'author', 'money_paid', 'payment_id', 'date_paid', 'paid_amount', 'buyer', 'money_transfer', 'date_transfer', 'transferred_amount')
